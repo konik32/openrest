527a0d2a55f8969f4fa520f32a35bcc20e62a022
konik32 <konickiszymon@gmail.com>
2015-08-03 21:42:12 +0200

    SpelEvaluationBeaTest package refactored

--- openrest/src/test/java/orest/dto/expression/spel/SpelEvaluationBeanTest.java
+++ openrest/src/test/java/orest/dto/expression/spel/SpelEvaluationBeanTest.java
@@ -1,13 +1,17 @@
-package orest.expression;
+package orest.dto.expression.spel;
 
 import lombok.Data;
+import orest.dto.expression.spel.DtoEvaluationWrapper;
+import orest.dto.expression.spel.SpelEvaluatorBean;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
+
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,7 +34,7 @@ public class SpelEvaluationBeanTest {
 	@Test
 	public void shouldSetValues() {
 		UserDto userDto = new UserDto();
-		spelEvaluatorBean.evaluate(userDto);
+		spelEvaluatorBean.evaluate(new DtoEvaluationWrapper(userDto, null));
 		assertEquals("staszek", userDto.getName());
 		assertEquals(true, userDto.getAuthenticated());
 		assertSame(100L, userDto.getId());
@@ -54,7 +58,7 @@ public class SpelEvaluationBeanTest {
 		@Value("#{@testBean.isAuthenticated()}")
 		private Boolean authenticated;
 		
-		@Value("#{target.number * 10}")
+		@Value("#{dto.number * 10}")
 		private Long id;
 		
 		private final Long number = 10L;
