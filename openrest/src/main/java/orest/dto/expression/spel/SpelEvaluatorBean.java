f35dd58555bed4ca836352824da106fd0c80ec1c
konik32 <konickiszymon@gmail.com>
2015-08-12 16:21:04 +0200

    Update SpelEvaluator to travers object graph

--- openrest/src/main/java/orest/dto/expression/spel/SpelEvaluatorBean.java
+++ openrest/src/main/java/orest/dto/expression/spel/SpelEvaluatorBean.java
@@ -1,15 +1,30 @@
 package orest.dto.expression.spel;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
 
+import orest.dto.Dto;
 import orest.dto.handler.DtoHandler;
+import orest.util.traverser.ObjectGraphTraverser;
+import orest.util.traverser.TraverserCallback;
+import orest.util.traverser.TraverserFieldFilter;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.util.ReflectionUtils.FieldCallback;
 
+/**
+ * This bean evaluates {@link Dto}'s field annotated with {@link Value}. Nested
+ * {@link Dto}s and parametarized {@link Iterable} of {@link Dto} are also
+ * evaluated.
+ * 
+ * @author Szymon Konicki
+ *
+ */
 public class SpelEvaluatorBean implements DtoHandler {
 
 	private final BeanFactory beanFactory;
@@ -20,19 +35,42 @@ public class SpelEvaluatorBean implements DtoHandler {
 	}
 
 	private void evaluate(final DtoEvaluationWrapper wrapper) {
-		final SpelEvaluator spelEvaluator = new SpelEvaluator(wrapper, beanFactory);
-		ReflectionUtils.doWithFields(wrapper.getDto().getClass(), new FieldCallback() {
+		ObjectGraphTraverser traverser = new ObjectGraphTraverser(new TraverserCallback() {
+			@Override
+			public void doWith(Field field, Object owner, String path) throws IllegalArgumentException,
+					IllegalAccessException {
+				final SpelEvaluator spelEvaluator = new SpelEvaluator(new DtoEvaluationWrapper(owner, null),
+						beanFactory);
+				Object value = spelEvaluator.evaluate(field);
+				field.set(owner, value);
+			}
+		}, new TraverserFieldFilter() {
 
 			@Override
-			public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
-				Value annotation = field.getAnnotation(Value.class);
-				if (annotation == null)
-					return;
-				Object value = spelEvaluator.evaluate(field);
-				ReflectionUtils.makeAccessible(field);
-				field.set(wrapper.getDto(), value);
+			public boolean matches(Field field, Object owner, String path) {
+				if (Iterable.class.isAssignableFrom(field.getType())) {
+					Type type = field.getGenericType();
+					if (type instanceof ParameterizedType) {
+						ParameterizedType pType = (ParameterizedType) type;
+						Type[] arr = pType.getActualTypeArguments();
+						return AnnotationUtils.isAnnotationDeclaredLocally(Dto.class, (Class<?>) arr[0]);
+					} else {
+						// Collections must be parameterized;
+						return false;
+					}
+				} else {
+					return AnnotationUtils.isAnnotationDeclaredLocally(Dto.class, field.getType());
+				}
+			}
+		}, new TraverserFieldFilter() {
+
+			@Override
+			public boolean matches(Field field, Object owner, String path) {
+				return field.isAnnotationPresent(Value.class);
 			}
 		});
+
+		traverser.traverse(wrapper.getDto());
 	}
 
 	@Override
@@ -45,4 +83,5 @@ public class SpelEvaluatorBean implements DtoHandler {
 		DtoEvaluationWrapper wrapper = new DtoEvaluationWrapper(dto, entity);
 		evaluate(wrapper);
 	}
+
 }
--- openrest/src/test/java/orest/dto/expression/spel/SpelEvaluationBeanTest.java
+++ openrest/src/test/java/orest/dto/expression/spel/SpelEvaluationBeanTest.java
@@ -1,17 +1,19 @@
 package orest.dto.expression.spel;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
 import lombok.Data;
-import orest.dto.expression.spel.DtoEvaluationWrapper;
-import orest.dto.expression.spel.SpelEvaluatorBean;
+import orest.dto.Dto;
+import orest.dto.Dto.DtoType;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
-
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,14 +36,27 @@ public class SpelEvaluationBeanTest {
 	@Test
 	public void shouldSetValues() {
 		UserDto userDto = new UserDto();
+		userDto.setAddresses(Arrays.asList(new Address()));
 		spelEvaluatorBean.handle(userDto, null);
 		assertEquals("staszek", userDto.getName());
 		assertEquals(true, userDto.getAuthenticated());
 		assertSame(100L, userDto.getId());
+		assertEquals("warszawa", userDto.getAddresses().get(0).getCity());
+		assertEquals("krakowska", userDto.getAddresses().get(0).getStreet());
+		assertEquals("warszawa krakowska", userDto.getAddresses().get(0).getWholeAddress());
 	}
-	
-	
-	
+
+	@Test
+	public void shouldNotEvaluatedNonDto() throws Exception {
+		// given
+		UserDto userDto = new UserDto();
+		userDto.setPersonalData(new PersonalData());
+		// when
+		spelEvaluatorBean.handle(userDto, null);
+		// then
+		assertNull(userDto.getPersonalData().getSurname());
+	}
+
 	public static class TestBean {
 
 		public Boolean isAuthenticated() {
@@ -50,20 +65,48 @@ public class SpelEvaluationBeanTest {
 	}
 
 	@Data
+	@Dto(entityType = Object.class, type = DtoType.CREATE)
 	public class UserDto {
 
 		@Value("#{'staszek'}")
 		private String name;
-		
+
 		@Value("#{@testBean.isAuthenticated()}")
 		private Boolean authenticated;
-		
+
 		@Value("#{dto.number * 10}")
 		private Long id;
-		
+
 		private final Long number = 10L;
+
+		private List<Address> addresses;
+
+		private PersonalData personalData;
 		
-		
-		
+
 	}
+
+	@Data
+	@Dto(entityType = Object.class, type = DtoType.CREATE)
+	public class Address {
+
+		@Value("#{'warszawa'}")
+		private String city;
+
+		@Value("#{'krakowska'}")
+		private String street;
+		
+		@Value("#{dto.city + ' ' + dto.street}")
+		private String wholeAddress;
+
+	}
+
+	@Data
+	public class PersonalData {
+
+		@Value("#{'Kowalski'}")
+		private String surname;
+
+	}
+
 }
