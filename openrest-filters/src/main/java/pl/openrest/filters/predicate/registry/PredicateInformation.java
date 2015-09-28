7e516e5ad9096ba1ee710d88fb93577e9e9bce5c
konik32 <konickiszymon@gmail.com>
2015-09-28 18:37:45 +0200

    Update PredicateInformation and PredicateRegistry not to hold static filters

--- openrest-filters/src/main/java/pl/openrest/filters/predicate/registry/PredicateInformation.java
+++ openrest-filters/src/main/java/pl/openrest/filters/predicate/registry/PredicateInformation.java
@@ -1,7 +1,7 @@
 package pl.openrest.filters.predicate.registry;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import lombok.EqualsAndHashCode;
@@ -9,8 +9,7 @@ import lombok.Getter;
 import lombok.ToString;
 import pl.openrest.filters.predicate.annotation.Predicate;
 import pl.openrest.filters.predicate.annotation.Predicate.PredicateType;
-import pl.openrest.filters.query.annotation.Join;
-import pl.openrest.filters.query.annotation.StaticFilter;
+import pl.openrest.filters.query.registry.JoinInformation;
 
 @Getter
 @ToString
@@ -21,13 +20,18 @@ public class PredicateInformation {
     private final Method method;
     private final PredicateType type;
     private final boolean defaultedPageable;
-    private final List<Join> joins;
+    private final List<JoinInformation> joins;
 
-    private PredicateInformation(Method method, Predicate predicateAnn) {
+    public PredicateInformation(Method method, Predicate predicateAnn, List<JoinInformation> joins) {
         this.name = predicateAnn.name();
         this.method = method;
         this.type = predicateAnn.type();
         this.defaultedPageable = predicateAnn.defaultedPageable();
-        this.joins = Arrays.asList(predicateAnn.joins());
+        this.joins = joins;
     }
+
+    public PredicateInformation(Method method, Predicate predicateAnn) {
+        this(method, predicateAnn, Collections.EMPTY_LIST);
+    }
+
 }
