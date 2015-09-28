f53445e06e31b515a460052ea56844cae1a5b4fc
konik32 <konickiszymon@gmail.com>
2015-09-28 09:22:06 +0200

    Create mappers interfaces

---
+++ openrest-dto/src/main/java/pl/openrest/dto/mapper/CreateMapper.java
@@ -0,0 +1,21 @@
+package pl.openrest.dto.mapper;
+
+import pl.openrest.dto.registry.DtoInformation;
+
+/**
+ * This interface should be implemented if there is a need for more control over
+ * creation of entity from dto, than simply mapping field by field as in
+ * {@link DefaultEntityFromDtoCreatorAndMerger}
+ * 
+ * @author Szymon Konicki
+ *
+ * @param <R>
+ *            Entity type
+ * @param <P>
+ *            Dto type
+ */
+public interface CreateMapper<R, P> {
+
+	R create(P from, DtoInformation dtoInfo);
+
+}
---
+++ openrest-dto/src/main/java/pl/openrest/dto/mapper/UpdateMapper.java
@@ -0,0 +1,18 @@
+package pl.openrest.dto.mapper;
+
+import pl.openrest.dto.registry.DtoInformation;
+
+/**
+ * This interface should be implemented if there is a need for more control over merging entity with dto, than simply mapping field by field
+ * as in {@link DefaultEntityFromDtoCreatorAndMerger}
+ * 
+ * @author Szymon Konicki
+ *
+ * @param <R>
+ *            Entity type
+ * @param <P>
+ *            Dto type
+ */
+public interface UpdateMapper<R, P> {
+    void merge(P from, R entity, DtoInformation dtoInfo);
+}
