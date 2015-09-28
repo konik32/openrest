4d7e1838e2f18aa48509b016f8fd38b61a06b9a8
konik32 <konickiszymon@gmail.com>
2015-09-28 18:37:05 +0200

    Add Predicate reference in doc

--- openrest-filters/src/main/java/pl/openrest/filters/query/annotation/Join.java
+++ openrest-filters/src/main/java/pl/openrest/filters/query/annotation/Join.java
@@ -5,9 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import pl.openrest.filters.predicate.annotation.Predicate;
+
 /**
- * Helper annotation to be used with {@link ExpressionMethod} specify joins that
- * will be created when {@link ExpressionMethod} is used in query. Association
+ * Helper annotation to be used with {@link Predicate} specify joins that
+ * will be created when {@link Predicate} is used in query. Association
  * specified in value will be left joined with entity.
  * 
  * @author Szymon Konicki
