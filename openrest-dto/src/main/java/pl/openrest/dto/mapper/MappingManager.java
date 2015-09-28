7575f030e41cb8c97141aba43c66438ead88c4cc
konik32 <konickiszymon@gmail.com>
2015-09-28 11:46:42 +0200

    Create MappingManager that manages dto to entity mapping and invokes mapping handlers

---
+++ openrest-dto/src/main/java/pl/openrest/dto/mapper/MappingManager.java
@@ -0,0 +1,131 @@
+package pl.openrest.dto.mapper;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import org.springframework.data.rest.core.invoke.RepositoryInvoker;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.util.StringUtils;
+
+import pl.openrest.dto.registry.DtoInformation;
+import pl.openrest.dto.registry.DtoInformationRegistry;
+import pl.openrest.exception.OrestException;
+import pl.openrest.exception.OrestExceptionDictionary;
+
+@SuppressWarnings({ "unchecked", "rawtypes" })
+public class MappingManager {
+
+    private static final String ERROR_MESSAGE = "Could not read an object of type %s from the request!";
+    private static final String NO_CONVERTER_FOUND = "No suitable HttpMessageConverter found to read request body into object of type %s from request with content type of %s!";
+    private final List<HttpMessageConverter<?>> messageConverters;
+    private final DtoInformationRegistry dtoInfoRegistry;
+    private final MapperDelegator mapperDelegator;
+
+    private List<BeforeCreateMappingHandler> beforeCreateHandlers = new ArrayList<BeforeCreateMappingHandler>();
+    private List<BeforeUpdateMappingHandler> beforeUpdateHandlers = new ArrayList<BeforeUpdateMappingHandler>();
+
+    public MappingManager(@NonNull MapperDelegator mapperDelegator, @NonNull List<HttpMessageConverter<?>> messageConverters,
+            @NonNull DtoInformationRegistry dtoInfoRegistry) {
+        this.mapperDelegator = mapperDelegator;
+        this.messageConverters = messageConverters;
+        this.dtoInfoRegistry = dtoInfoRegistry;
+    }
+
+    public DtoAndEntityWrapper create(MediaType contentType, HttpInputMessage inputMessage, String dtoParam) {
+        DtoInformation dtoInfo = getDtoInformation(dtoParam);
+        Object dto = convertToDto(contentType, inputMessage, dtoInfo.getDtoType());
+
+        handle(dto);
+
+        Object entity = mapperDelegator.create(dto, dtoInfo);
+        return new DtoAndEntityWrapper(dto, entity);
+    }
+
+    public DtoAndEntityWrapper merge(MediaType contentType, HttpInputMessage inputMessage, RepositoryInvoker invoker, Serializable id,
+            String dtoParam) {
+
+        DtoInformation dtoInfo = getDtoInformation(dtoParam);
+        Object dto = convertToDto(contentType, inputMessage, dtoInfo.getDtoType());
+
+        Object existingObject = invoker.invokeFindOne(id);
+
+        if (existingObject == null) {
+            throw new ResourceNotFoundException();
+        }
+
+        handle(dto, existingObject);
+
+        mapperDelegator.merge(dto, existingObject, dtoInfo);
+        return new DtoAndEntityWrapper(dto, existingObject);
+    }
+
+    private void handle(Object dto) {
+        for (BeforeCreateMappingHandler handler : beforeCreateHandlers)
+            handler.handle(dto);
+    }
+
+    private void handle(Object dto, Object entity) {
+        for (BeforeUpdateMappingHandler handler : beforeUpdateHandlers)
+            handler.handle(dto, entity);
+    }
+
+    public void addHandler(BeforeCreateMappingHandler handler) {
+        beforeCreateHandlers.add(handler);
+    }
+
+    public void addHandler(BeforeUpdateMappingHandler handler) {
+        beforeUpdateHandlers.add(handler);
+    }
+
+    private Object convertToDto(MediaType contentType, HttpInputMessage inputMessage, Class<?> dtoType) {
+        for (HttpMessageConverter converter : messageConverters) {
+
+            if (!converter.canRead(PersistentEntityResource.class, contentType)) {
+                continue;
+            }
+
+            Object dto = read(inputMessage, converter, dtoType);
+            if (dto == null) {
+                throw new HttpMessageNotReadableException(String.format(ERROR_MESSAGE, dtoType));
+            }
+            return dto;
+        }
+        throw new HttpMessageNotReadableException(String.format(NO_CONVERTER_FOUND, dtoType, contentType));
+    }
+
+    private DtoInformation getDtoInformation(String dtoParam) {
+        if (!StringUtils.hasText(dtoParam))
+            throw new OrestException(OrestExceptionDictionary.NO_DTO_PARAMETER, "Request must contain dto parameter");
+        DtoInformation dtoInfo = dtoInfoRegistry.get(dtoParam);
+        if (dtoInfo == null)
+            throw new OrestException(OrestExceptionDictionary.NO_SUCH_DTO, "There is no such dto: " + dtoParam + " defined");
+        if (!dtoInfo.isExported())
+            throw new OrestException(OrestExceptionDictionary.DTO_NOT_EXPORTED, dtoParam + " is not exported");
+        return dtoInfo;
+    }
+
+    private Object read(HttpInputMessage inputMessage, HttpMessageConverter<Object> converter, Class<?> type) {
+        try {
+            return converter.read(type, inputMessage);
+        } catch (IOException e) {
+            throw new HttpMessageNotReadableException(String.format(ERROR_MESSAGE, type));
+        }
+    }
+
+    @Data
+    public static class DtoAndEntityWrapper {
+        private final Object dto;
+        private final Object entity;
+    }
+
+}
---
+++ openrest-dto/src/test/java/pl/openrest/dto/mapper/MappingManagerTest.java
@@ -0,0 +1,199 @@
+package pl.openrest.dto.mapper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.rest.core.invoke.RepositoryInvoker;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
+import pl.openrest.dto.annotations.Dto;
+import pl.openrest.dto.dummy.model.User;
+import pl.openrest.dto.dummy.model.dto.UserDto;
+import pl.openrest.dto.mapper.MappingManager.DtoAndEntityWrapper;
+import pl.openrest.dto.registry.DtoInformation;
+import pl.openrest.dto.registry.DtoInformationRegistry;
+import pl.openrest.exception.OrestException;
+
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings("unchecked")
+public class MappingManagerTest {
+
+    @Mock
+    private MapperDelegator mapperDelegator;
+
+    @Mock
+    private DtoInformationRegistry dtoDomainRegistry;
+    @Mock
+    private HttpInputMessage inputMessage;
+
+    @Mock
+    private RepositoryInvoker invoker;
+    @Mock
+    private HttpMessageConverter converter;
+
+    private MediaType contentType = MediaType.APPLICATION_JSON;
+
+    private Serializable id = 1l;
+    private String dtoParam = "dto";
+
+    private MappingManager mappingManager;
+
+    private DtoInformation dtoInfo;
+
+    @Mock
+    private UserDto userDto;
+
+    @Mock
+    private User user;
+
+    @Mock
+    private BeforeCreateMappingHandler beforeCreateHandler;
+    @Mock
+    private BeforeUpdateMappingHandler beforeUpdateHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+        messageConverters.add(converter);
+
+        mappingManager = new MappingManager(mapperDelegator, messageConverters, dtoDomainRegistry);
+        mappingManager.addHandler(beforeCreateHandler);
+        mappingManager.addHandler(beforeUpdateHandler);
+        dtoInfo = new DtoInformation(UserDto.class, UserDto.class.getAnnotation(Dto.class));
+
+        when(dtoDomainRegistry.get(dtoParam)).thenReturn(dtoInfo);
+        when(converter.canRead(PersistentEntityResource.class, contentType)).thenReturn(true);
+        when(converter.read(UserDto.class, inputMessage)).thenReturn(userDto);
+        when(invoker.invokeFindOne(id)).thenReturn(user);
+        when(mapperDelegator.create(userDto, dtoInfo)).thenReturn(user);
+    }
+
+    @Test
+    public void shouldCallDtoDomainRegistry() throws Exception {
+        // given
+        // when
+        mappingManager.create(contentType, inputMessage, dtoParam);
+        mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        // then
+        verify(dtoDomainRegistry, times(2)).get(dtoParam);
+    }
+
+    @Test
+    public void shouldCallConverterRead() throws Exception {
+        mappingManager.create(contentType, inputMessage, dtoParam);
+        mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        verify(converter, times(2)).read(UserDto.class, inputMessage);
+    }
+
+    @Test
+    public void shouldCallEntityFromDtoCreatorCreate() throws Exception {
+        mappingManager.create(contentType, inputMessage, dtoParam);
+        verify(mapperDelegator, times(1)).create(userDto, dtoInfo);
+    }
+
+    @Test
+    public void shouldCallEntityFromDtoCreatorMerge() throws Exception {
+        mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        verify(mapperDelegator, times(1)).merge(userDto, user, dtoInfo);
+    }
+
+    @Test
+    public void shouldCallDtoHandler() throws Exception {
+        mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        mappingManager.create(contentType, inputMessage, dtoParam);
+        verify(beforeUpdateHandler, times(1)).handle(userDto, user);
+        verify(beforeCreateHandler, times(1)).handle(userDto);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void shouldThrowResourceNotFoundExceptionOnInvokeFindOneNull() throws Exception {
+        // given
+        when(invoker.invokeFindOne(id)).thenReturn(null);
+        // when
+        mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        // then
+    }
+
+    @Test(expected = HttpMessageNotReadableException.class)
+    public void shouldThrowHttpMessageNotReadableExceptionOnConverterReadsNull() throws Exception {
+        // given
+        when(converter.read(UserDto.class, inputMessage)).thenReturn(null);
+        // when
+        mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        // then
+    }
+
+    @Test
+    public void shouldReturnDtoAndEntityWrapper() throws Exception {
+        // given
+        // when
+        DtoAndEntityWrapper wrapperMerge = mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        DtoAndEntityWrapper wrapperCreate = mappingManager.create(contentType, inputMessage, dtoParam);
+        // then
+        assertNotNull(wrapperMerge);
+        assertNotNull(wrapperMerge.getDto());
+        assertNotNull(wrapperMerge.getEntity());
+        assertEquals(userDto, wrapperMerge.getDto());
+        assertEquals(user, wrapperMerge.getEntity());
+
+        assertNotNull(wrapperCreate);
+        assertNotNull(wrapperCreate.getDto());
+        assertNotNull(wrapperCreate.getEntity());
+        assertEquals(userDto, wrapperCreate.getDto());
+        assertEquals(user, wrapperCreate.getEntity());
+    }
+
+    @Test(expected = OrestException.class)
+    public void shouldThrowOrestExceptionIfDtoParameterIsEmpty() throws Exception {
+        // when
+        mappingManager.merge(contentType, inputMessage, invoker, id, "");
+        // then
+    }
+
+    @Test(expected = OrestException.class)
+    public void shouldThrowOrestExceptionIfNoDtoFound() throws Exception {
+        // given
+        when(dtoDomainRegistry.get(dtoParam)).thenReturn(null);
+        // when
+        mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        // then
+    }
+
+    @Test(expected = HttpMessageNotReadableException.class)
+    public void shouldThrowHttpMessageNotReadableExceptionOnNoConverterFoundForDtoType() throws Exception {
+        // given
+        when(converter.canRead(PersistentEntityResource.class, contentType)).thenReturn(false);
+        // when
+        mappingManager.merge(contentType, inputMessage, invoker, id, dtoParam);
+        // then
+    }
+
+    @Test(expected = OrestException.class)
+    public void shouldThrowOrestExceptionOnDtoNotExported() {
+        // given
+        DtoInformation dtoInfo = mock(DtoInformation.class);
+        when(dtoInfo.isExported()).thenReturn(false);
+        when(dtoDomainRegistry.get(dtoParam)).thenReturn(dtoInfo);
+        // when
+        mappingManager.create(contentType, inputMessage, dtoParam);
+    }
+
+}
