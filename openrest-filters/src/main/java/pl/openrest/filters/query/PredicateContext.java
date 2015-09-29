7eae2d75047b3b9289100f0196bb029c7d88bbcf
konik32 <konickiszymon@gmail.com>
2015-09-29 17:24:31 +0200

    Update PredicateContext to be simple pojo

--- openrest-filters/src/main/java/pl/openrest/filters/query/PredicateContext.java
+++ openrest-filters/src/main/java/pl/openrest/filters/query/PredicateContext.java
@@ -1,33 +1,23 @@
 package pl.openrest.filters.query;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import pl.openrest.filters.query.registry.JoinInformation;
 
 import com.mysema.query.types.Predicate;
 
+@RequiredArgsConstructor
 @ToString
 @EqualsAndHashCode
 public class PredicateContext {
 
-    private List<JoinInformation> joins = new ArrayList<JoinInformation>();
-    private @Getter @Setter Predicate predicate;
-
-    public void addJoin(JoinInformation join) {
-        if (join != null)
-            joins.add(join);
-    }
-
-    public void addJoins(List<JoinInformation> joins) {
-        if (joins != null)
-            this.joins.addAll(joins);
-    }
+    private final List<JoinInformation> joins;
+    private final @Getter Predicate predicate;
 
     public List<JoinInformation> getJoins() {
         return Collections.unmodifiableList(joins);
