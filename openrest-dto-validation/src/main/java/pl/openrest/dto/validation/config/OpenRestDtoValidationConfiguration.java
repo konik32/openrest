f40ee9bc30b24ef4b93c6c9d1eb35655de1b004a
konik32 <konickiszymon@gmail.com>
2015-09-30 15:05:18 +0200

    Add request scoped ValidationContext and ValidationContextHandler

--- openrest-dto-validation/src/main/java/pl/openrest/dto/validation/config/OpenRestDtoValidationConfiguration.java
+++ openrest-dto-validation/src/main/java/pl/openrest/dto/validation/config/OpenRestDtoValidationConfiguration.java
@@ -3,12 +3,15 @@ package pl.openrest.dto.validation.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.validation.Validator;
 
 import pl.openrest.dto.mapper.BeforeCreateMappingHandler;
 import pl.openrest.dto.mapper.BeforeUpdateMappingHandler;
 import pl.openrest.dto.mapper.MappingManager;
 import pl.openrest.dto.validation.DtoFieldExpressionValidator;
+import pl.openrest.dto.validation.handler.ValidationContext;
 import pl.openrest.dto.validation.handler.ValidationContextHandler;
 import pl.openrest.dto.validation.handler.ValidatorInvoker;
 
@@ -27,11 +30,10 @@ public class OpenRestDtoValidationConfiguration {
     }
 
     @Autowired
-    public void addDtoValidatorInvokerHandler(MappingManager mappingManager) {
+    public void addDtoValidatorInvokerHandler(MappingManager mappingManager, ValidationContextHandler validationContextHandler) {
         ValidatorInvoker validatorInvoker = validatorInvoker();
-        ValidationContextHandler contextHandler = new ValidationContextHandler();
-        mappingManager.addHandler((BeforeCreateMappingHandler) contextHandler);
-        mappingManager.addHandler((BeforeUpdateMappingHandler) contextHandler);
+        mappingManager.addHandler((BeforeCreateMappingHandler) validationContextHandler);
+        mappingManager.addHandler((BeforeUpdateMappingHandler) validationContextHandler);
         mappingManager.addHandler((BeforeCreateMappingHandler) validatorInvoker);
         mappingManager.addHandler((BeforeUpdateMappingHandler) validatorInvoker);
     }
@@ -42,6 +44,17 @@ public class OpenRestDtoValidationConfiguration {
         if (dtoFieldExpressionValidator != null) {
             invoker.addValidator(dtoFieldExpressionValidator);
         }
+    }
+
+    @Bean
+    @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public ValidationContext validationContext() {
+        return new ValidationContext();
+    }
+
+    @Bean
+    public ValidationContextHandler validationContextHandler() {
+        return new ValidationContextHandler();
     }
 
 }
