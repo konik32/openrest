772ad2ee0cdd0fe47ac8c0598407e44badcc4123
konik32 <konickiszymon@gmail.com>
2015-10-01 16:04:58 +0200

    Update FilterableEntityController and FilterableSearchController to use ResourceResolver and QuereMethodExecutor

--- openrest-filters/src/main/java/org/springframework/data/rest/webmvc/FilterableEntityController.java
+++ openrest-filters/src/main/java/org/springframework/data/rest/webmvc/FilterableEntityController.java
@@ -1,92 +1,161 @@
 package org.springframework.data.rest.webmvc;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import lombok.NonNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.querydsl.QSort;
-import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.data.rest.webmvc.RootResourceInformation;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.data.rest.core.mapping.ResourceMetadata;
+import org.springframework.data.rest.core.mapping.SearchResourceMappings;
 import org.springframework.data.rest.webmvc.support.BackendId;
 import org.springframework.data.rest.webmvc.support.DefaultedPageable;
+import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import pl.openrest.filters.domain.registry.FilterableEntityInformation;
-import pl.openrest.filters.query.PredicateContext;
-import pl.openrest.filters.query.PredicateContextBuilderFactory;
-import pl.openrest.filters.query.PredicateContextBuilderFactory.PredicateContextBuilder;
-import pl.openrest.filters.repository.PredicateContextRepositoryInvoker;
 import pl.openrest.filters.webmvc.CountResponse;
-import pl.openrest.predicate.parser.FilterTreeBuilder;
+import pl.openrest.filters.webmvc.ResourceResolver;
 
 @RepositoryRestController
-public class FilterableEntityController extends AbstractFilterablesController {
+public class FilterableEntityController extends AbstractRepositoryRestController {
 
-    private final static String BASE_MAPPING = "/{repository}";
+    private static final String BASE_MAPPING = "/{repository}";
+
+    private final RepositoryEntityLinks entityLinks;
+    private final ResourceResolver resourceResolver;
 
     @Autowired
-    public FilterableEntityController(PagedResourcesAssembler<Object> pagedResourcesAssembler,
-            PredicateContextBuilderFactory predicateContextBuilderFactory, FilterTreeBuilder filterTreeBuilder) {
-        super(pagedResourcesAssembler, predicateContextBuilderFactory, filterTreeBuilder);
+    public FilterableEntityController(Repositories repositories, RepositoryEntityLinks entityLinks,
+            PagedResourcesAssembler<Object> assembler, @NonNull ResourceResolver resourceResolver) {
+
+        super(assembler);
+
+        this.entityLinks = entityLinks;
+        this.resourceResolver = resourceResolver;
+    }
+
+    /**
+     * <code>GET /{repository}</code> - Returns the collection resource (paged or unpaged).
+     * 
+     * @param resourceInformation
+     * @param pageable
+     * @param sort
+     * @param assembler
+     * @return
+     * @throws ResourceNotFoundException
+     * @throws HttpRequestMethodNotSupportedException
+     */
+    @ResponseBody
+    @RequestMapping(value = BASE_MAPPING, method = RequestMethod.GET, params = "orest")
+    public Resources<?> getCollectionResource(final RootResourceInformation resourceInformation,
+            @RequestParam MultiValueMap<String, Object> parameters, DefaultedPageable pageable, Sort sort,
+            PersistentEntityResourceAssembler assembler) throws ResourceNotFoundException, HttpRequestMethodNotSupportedException {
+
+        resourceInformation.verifySupportedMethod(HttpMethod.GET, ResourceType.COLLECTION);
+
+        Iterable<?> results = resourceResolver.getCollectionResource(resourceInformation, parameters, pageable, sort);
+
+        // In Spring Data Rest RepositoryEntityController
+        // RepositoryInvoker invoker = resourceInformation.getInvoker();
+        //
+        // if (null == invoker) {
+        // throw new ResourceNotFoundException();
+        // }
+        //
+        // Iterable<?> results;
+        //
+        // if (pageable.getPageable() != null) {
+        // results = invoker.invokeFindAll(pageable.getPageable());
+        // } else {
+        // results = invoker.invokeFindAll(sort);
+        // }
+
+        ResourceMetadata metadata = resourceInformation.getResourceMetadata();
+        SearchResourceMappings searchMappings = metadata.getSearchResourceMappings();
+        List<Link> links = new ArrayList<Link>();
+
+        if (searchMappings.isExported()) {
+            links.add(entityLinks.linkFor(metadata.getDomainType()).slash(searchMappings.getPath()).withRel(searchMappings.getRel()));
+        }
+
+        Link baseLink = entityLinks.linkToPagedResource(resourceInformation.getDomainType(),
+                pageable.isDefault() ? null : pageable.getPageable());
+
+        Resources<?> resources = resultToResources(results, assembler, baseLink);
+        resources.add(links);
+        return resources;
     }
 
     @ResponseBody
-    @RequestMapping(value = BASE_MAPPING, method = RequestMethod.GET, params = "orest")
-    public ResponseEntity<Object> getFilteredCollectionResource(RootResourceInformation resourceInformation,
-            FilterableEntityInformation entityInfo, @RequestParam MultiValueMap<String, Object> parameters, DefaultedPageable pageable,
-            QSort sort, PersistentEntityResourceAssembler assembler) {
+    @SuppressWarnings({ "unchecked" })
+    @RequestMapping(value = BASE_MAPPING, method = RequestMethod.GET, produces = { "application/x-spring-data-compact+json",
+            "text/uri-list" }, params = "orest")
+    public Resources<?> getCollectionResourceCompact(RootResourceInformation repoRequest,
+            @RequestParam MultiValueMap<String, Object> parameters, DefaultedPageable pageable, Sort sort,
+            PersistentEntityResourceAssembler assembler) throws ResourceNotFoundException, HttpRequestMethodNotSupportedException {
 
-        PredicateContextBuilder predicateContextBuilder = predicateContextBuilderFactory.create(entityInfo);
+        Resources<?> resources = getCollectionResource(repoRequest, parameters, pageable, sort, assembler);
 
-        addFilters(parameters, predicateContextBuilder);
-        predicateContextBuilder.withStaticFilters();
+        List<Link> links = new ArrayList<Link>(resources.getLinks());
 
-        boolean addDefaultPageable = checkIfAddDefaultPageable(entityInfo.isDefaultedPageable());
+        for (Resource<?> resource : ((Resources<Resource<?>>) resources).getContent()) {
+            PersistentEntityResource persistentEntityResource = (PersistentEntityResource) resource;
+            links.add(resourceLink(repoRequest, persistentEntityResource));
+        }
+        if (resources instanceof PagedResources) {
+            return new PagedResources<Object>(Collections.emptyList(), ((PagedResources<?>) resources).getMetadata(), links);
+        } else {
+            return new Resources<Object>(Collections.emptyList(), links);
+        }
+    }
 
-        Iterable<Object> result = getResult(entityInfo.getRepositoryInvoker(), predicateContextBuilder.build(), pageable, sort,
-                addDefaultPageable);
-        return new ResponseEntity<Object>(resultToResources(result, assembler, null), HttpStatus.OK);
+    /**
+     * <code>GET /{repository}/{id}</code> - Returns a single entity.
+     * 
+     * @param resourceInformation
+     * @param id
+     * @return
+     * @throws HttpRequestMethodNotSupportedException
+     */
+    @RequestMapping(value = BASE_MAPPING + "/{id}", method = RequestMethod.GET, params = "orest")
+    public ResponseEntity<Resource<?>> getItemResource(RootResourceInformation resourceInformation, @BackendId Serializable id,
+            @RequestParam MultiValueMap<String, Object> parameters, PersistentEntityResourceAssembler assembler)
+            throws HttpRequestMethodNotSupportedException {
+
+        Object domainObj = resourceResolver.getItemResource(resourceInformation, parameters, id);
+        // In Spring Data Rest RepositoryEntityController
+        // Object domainObj = getItemResource(resourceInformation, id);
+
+        if (domainObj == null) {
+            return new ResponseEntity<Resource<?>>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Resource<?>>(assembler.toFullResource(domainObj), HttpStatus.OK);
     }
 
     @ResponseBody
     @RequestMapping(value = BASE_MAPPING, method = RequestMethod.GET, params = { "orest", "count" })
     public ResponseEntity<Object> getFilteredCollectionCount(RootResourceInformation resourceInformation,
-            FilterableEntityInformation entityInfo, @RequestParam MultiValueMap<String, Object> parameters) {
-
-        PredicateContextBuilder predicateContextBuilder = predicateContextBuilderFactory.create(entityInfo);
-
-        addFilters(parameters, predicateContextBuilder);
-        predicateContextBuilder.withStaticFilters();
-
-        Object result = getCountResult(entityInfo.getRepositoryInvoker(), predicateContextBuilder.build());
+            @RequestParam MultiValueMap<String, Object> parameters) {
+        Object result = resourceResolver.getCollectionCount(resourceInformation, parameters);
         return new ResponseEntity<Object>(new CountResponse(result), HttpStatus.OK);
-    }
-
-    @ResponseBody
-    @RequestMapping(value = BASE_MAPPING + "/{id}", method = RequestMethod.GET, params = "orest")
-    public ResponseEntity<Object> getFilteredResource(RootResourceInformation resourceInformation, FilterableEntityInformation entityInfo,
-            @RequestParam MultiValueMap<String, Object> parameters, @BackendId Serializable id, PersistentEntityResourceAssembler assembler) {
-
-        PredicateContextBuilder predicateContextBuilder = predicateContextBuilderFactory.create(entityInfo);
-        predicateContextBuilder.withId(resourceInformation.getPersistentEntity().getIdProperty(), id);
-        predicateContextBuilder.withStaticFilters();
-        addFilters(parameters, predicateContextBuilder);
-
-        Object result = getResult(entityInfo.getRepositoryInvoker(), predicateContextBuilder.build());
-        if (result == null)
-            throw new ResourceNotFoundException();
-        return new ResponseEntity<Object>(assembler.toFullResource(result), HttpStatus.OK);
-    }
-
-    private Object getResult(PredicateContextRepositoryInvoker invoker, PredicateContext predicateContext) {
-        return invoker.invokeFindOne(predicateContext);
     }
 
 }
--- openrest-filters/src/main/java/org/springframework/data/rest/webmvc/FilterableSearchController.java
+++ openrest-filters/src/main/java/org/springframework/data/rest/webmvc/FilterableSearchController.java
@@ -1,14 +1,22 @@
 package org.springframework.data.rest.webmvc;
 
+import static org.springframework.data.rest.webmvc.ControllerUtils.EMPTY_RESOURCE_LIST;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.NonNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.querydsl.QSort;
-import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.data.rest.webmvc.RootResourceInformation;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.rest.core.mapping.ResourceMappings;
 import org.springframework.data.rest.webmvc.support.DefaultedPageable;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
@@ -17,65 +25,108 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
-import pl.openrest.filters.domain.registry.FilterableEntityInformation;
-import pl.openrest.filters.predicate.registry.PredicateInformation;
-import pl.openrest.filters.query.PredicateContextBuilderFactory;
-import pl.openrest.filters.query.PredicateContextBuilderFactory.PredicateContextBuilder;
 import pl.openrest.filters.webmvc.CountResponse;
-import pl.openrest.predicate.parser.FilterTreeBuilder;
-import pl.openrest.predicate.parser.PredicateParts;
-import pl.openrest.predicate.parser.PredicatePartsExtractor;
+import pl.openrest.filters.webmvc.QueryMethodExecutor;
 
 @RepositoryRestController
-public class FilterableSearchController extends AbstractFilterablesController {
+public class FilterableSearchController extends AbstractRepositoryRestController {
 
-    private final PredicatePartsExtractor predicatePartsExtractor;
+    private static final String SEARCH = "/search";
+    private static final String BASE_MAPPING = "/{repository}" + SEARCH;
 
-    private final static String BASE_MAPPING = "/{repository}/search";
+    private final QueryMethodExecutor queryMethodExecutor;
 
+    /**
+     * Creates a new {@link RepositorySearchController} using the given {@link PagedResourcesAssembler}, {@link EntityLinks} and
+     * {@link ResourceMappings}.
+     * 
+     * @param assembler
+     *            must not be {@literal null}.
+     * @param entityLinks
+     *            must not be {@literal null}.
+     * @param mappings
+     *            must not be {@literal null}.
+     */
     @Autowired
-    public FilterableSearchController(PagedResourcesAssembler<Object> pagedResourcesAssembler,
-            PredicateContextBuilderFactory predicateContextBuilderFactory, FilterTreeBuilder filterTreeBuilder,
-            @NonNull PredicatePartsExtractor predicatePartsExtractor) {
-        super(pagedResourcesAssembler, predicateContextBuilderFactory, filterTreeBuilder);
-        this.predicatePartsExtractor = predicatePartsExtractor;
+    public FilterableSearchController(PagedResourcesAssembler<Object> assembler, @NonNull QueryMethodExecutor queryMethodExecutor) {
+
+        super(assembler);
+
+        this.queryMethodExecutor = queryMethodExecutor;
     }
 
+    /**
+     * Executes the search with the given name.
+     * 
+     * @param request
+     * @param repository
+     * @param search
+     * @param pageable
+     * @return
+     * @throws ResourceNotFoundException
+     */
     @ResponseBody
     @RequestMapping(value = BASE_MAPPING + "/{search}", method = RequestMethod.GET, params = "orest")
-    public ResponseEntity<Object> executeSearch(RootResourceInformation resourceInformation, FilterableEntityInformation entityInfo,
-            @RequestParam MultiValueMap<String, Object> parameters, @PathVariable String search, DefaultedPageable pageable, QSort sort,
+    public ResponseEntity<Object> executeSearch(RootResourceInformation resourceInformation, WebRequest request,
+            @PathVariable String search, @RequestParam MultiValueMap<String, Object> parameters, DefaultedPageable pageable, Sort sort,
             PersistentEntityResourceAssembler assembler) {
 
-        PredicateParts searchPredicateParts = predicatePartsExtractor.extractParts(search);
-        PredicateContextBuilder predicateContextBuilder = predicateContextBuilderFactory.create(entityInfo);
+        Object result = queryMethodExecutor.executeQueryMethod(resourceInformation, parameters, search, pageable, sort);
+        // In Spring Data Rest RepositoryEntityController
+        // Method method = checkExecutability(resourceInformation, search);
+        // Object resources = executeQueryMethod(resourceInformation.getInvoker(), request, method, pageable, sort, assembler);
 
-        predicateContextBuilder.withPredicateParts(searchPredicateParts);
-        predicateContextBuilder.withStaticFilters();
-        addFilters(parameters, predicateContextBuilder);
-
-        PredicateInformation searchPredicateInfo = entityInfo.getPredicateInformation(searchPredicateParts.getPredicateName());
-        boolean addDefaultPageable = checkIfAddDefaultPageable(entityInfo.isDefaultedPageable(), searchPredicateInfo.isDefaultedPageable());
-
-        Iterable<Object> result = getResult(entityInfo.getRepositoryInvoker(), predicateContextBuilder.build(), pageable, sort,
-                addDefaultPageable);
+        // return new ResponseEntity<Object>(resources, HttpStatus.OK);
         return new ResponseEntity<Object>(resultToResources(result, assembler, null), HttpStatus.OK);
     }
 
+    /**
+     * Executes a query method and exposes the results in compact form.
+     * 
+     * @param resourceInformation
+     * @param repository
+     * @param method
+     * @param pageable
+     * @return
+     */
     @ResponseBody
-    @RequestMapping(value = BASE_MAPPING + "/{search}", method = RequestMethod.GET, params = {"orest", "count"})
-    public ResponseEntity<Object> executeSearchCount(RootResourceInformation resourceInformation, FilterableEntityInformation entityInfo,
+    @RequestMapping(value = BASE_MAPPING + "/{search}", method = RequestMethod.GET, //
+    produces = { "application/x-spring-data-compact+json" }, params = "orest")
+    public ResourceSupport executeSearchCompact(RootResourceInformation resourceInformation, WebRequest request,
+            @PathVariable String repository, @PathVariable String search, @RequestParam MultiValueMap<String, Object> parameters,
+            DefaultedPageable pageable, Sort sort, PersistentEntityResourceAssembler assembler) {
+        // In Spring Data Rest RepositoryEntityController
+        // Method method = checkExecutability(resourceInformation, search);
+        // Object resource = executeQueryMethod(resourceInformation.getInvoker(), request, method, pageable, sort, assembler);
+        Object result = queryMethodExecutor.executeQueryMethod(resourceInformation, parameters, search, pageable, sort);
+        Object resource = resultToResources(result, assembler, null);
+        List<Link> links = new ArrayList<Link>();
+
+        if (resource instanceof Resources && ((Resources<?>) resource).getContent() != null) {
+
+            for (Object obj : ((Resources<?>) resource).getContent()) {
+                if (null != obj && obj instanceof Resource) {
+                    Resource<?> res = (Resource<?>) obj;
+                    links.add(resourceLink(resourceInformation, res));
+                }
+            }
+
+        } else if (resource instanceof Resource) {
+
+            Resource<?> res = (Resource<?>) resource;
+            links.add(resourceLink(resourceInformation, res));
+        }
+
+        return new Resources<Resource<?>>(EMPTY_RESOURCE_LIST, links);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = BASE_MAPPING + "/{search}", method = RequestMethod.GET, params = { "orest", "count" })
+    public ResponseEntity<Object> executeSearchCount(RootResourceInformation resourceInformation,
             @RequestParam MultiValueMap<String, Object> parameters, @PathVariable String search) {
-
-        PredicateParts searchPredicateParts = predicatePartsExtractor.extractParts(search);
-        PredicateContextBuilder predicateContextBuilder = predicateContextBuilderFactory.create(entityInfo);
-
-        predicateContextBuilder.withPredicateParts(searchPredicateParts);
-        predicateContextBuilder.withStaticFilters();
-        addFilters(parameters, predicateContextBuilder);
-
-        Object result = getCountResult(entityInfo.getRepositoryInvoker(), predicateContextBuilder.build());
+        Object result = queryMethodExecutor.executeCountQueryMethod(resourceInformation, parameters, search);
         return new ResponseEntity<Object>(new CountResponse(result), HttpStatus.OK);
     }
 
