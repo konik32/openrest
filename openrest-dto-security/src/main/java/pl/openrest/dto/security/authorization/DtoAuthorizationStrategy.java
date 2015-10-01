75571e6ba407dd70732acfe117f9b23102953124
konik32 <konickiszymon@gmail.com>
2015-10-01 17:00:56 +0200

    Shorten generics names

--- openrest-dto-security/src/main/java/pl/openrest/dto/security/authorization/DtoAuthorizationStrategy.java
+++ openrest-dto-security/src/main/java/pl/openrest/dto/security/authorization/DtoAuthorizationStrategy.java
@@ -1,9 +1,11 @@
 package pl.openrest.dto.security.authorization;
 
-public interface DtoAuthorizationStrategy<PRINCIPAL, DTO, ENTITY> {
-    int isAuthorized(PRINCIPAL principal, DTO dto, ENTITY entity);
+public interface DtoAuthorizationStrategy<P, D, E> {
 
     public static final int GRANT = 1;
     public static final int ABSTAIN = 0;
     public static final int DENY = -1;
+
+    int isAuthorized(P principal, D dto, E entity);
+
 }
