7d20e4e76f1b765daa1c0d5157b070f608aafdc1
konik32 <konickiszymon@gmail.com>
2015-10-14 15:03:45 +0200

    Add PredicateInformationFactory that constructs AbstractPredicates

---
+++ openrest-rpr-generator/src/main/java/pl/openrest/filters/generator/predicate/context/PredicateInformationFactory.java
@@ -0,0 +1,63 @@
+package pl.openrest.filters.generator.predicate.context;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.List;
+
+import lombok.Data;
+
+import org.reflections.Reflections;
+
+import pl.openrest.filters.client.predicate.AbstractPredicate;
+import pl.openrest.filters.client.predicate.FilterPredicate;
+import pl.openrest.filters.client.predicate.SearchPredicate;
+import pl.openrest.filters.client.predicate.SortPredicate;
+import pl.openrest.filters.predicate.annotation.Predicate;
+import pl.openrest.filters.predicate.annotation.Predicate.PredicateType;
+
+public class PredicateInformationFactory {
+
+    private final Reflections reflections;
+
+    public PredicateInformationFactory(Reflections reflections) {
+        this.reflections = reflections;
+    }
+
+    public AbstractPredicate from(Method method) {
+        Predicate predicateAnn = method.getAnnotation(Predicate.class);
+        if (predicateAnn == null)
+            throw new IllegalArgumentException("Method should be annotated with @Predicate");
+
+        String name = predicateAnn.name().isEmpty() ? method.getName() : predicateAnn.name();
+        PredicateType type = predicateAnn.type();
+
+        switch (type) {
+        case SEARCH:
+            boolean defaultedPageable = predicateAnn.defaultedPageable();
+            return new SearchPredicate(name, defaultedPageable, createParametersInformation(method));
+        case FILTER:
+            return new FilterPredicate(name, createParametersInformation(method));
+        case SORT:
+            return new SortPredicate(name, createParametersInformation(method));
+        default:
+            // should never get here
+            return null;
+        }
+    }
+
+    private ParameterInformation[] createParametersInformation(Method method) {
+        Parameter parameters[] = method.getParameters();
+        List<String> names = reflections.getMethodParamNames(method);
+        ParameterInformation parametersInfo[] = new ParameterInformation[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+            parametersInfo[i] = new ParameterInformation(names.get(i), parameters[i].getType());
+        }
+        return parametersInfo;
+    }
+
+    @Data
+    public static class ParameterInformation {
+        private final String name;
+        private final Class<?> type;
+    }
+}
---
+++ openrest-rpr-generator/src/test/java/pl/openrest/filters/generator/predicate/context/PredicateInformationFactoryTest.java
@@ -0,0 +1,115 @@
+package pl.openrest.filters.generator.predicate.context;
+
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.reflections.Reflections;
+
+import pl.openrest.filters.client.predicate.AbstractPredicate;
+import pl.openrest.filters.client.predicate.FilterPredicate;
+import pl.openrest.filters.client.predicate.SearchPredicate;
+import pl.openrest.filters.client.predicate.SortPredicate;
+import pl.openrest.filters.generator.predicate.context.PredicateInformationFactory.ParameterInformation;
+import pl.openrest.filters.predicate.annotation.Predicate;
+import pl.openrest.filters.predicate.annotation.Predicate.PredicateType;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PredicateInformationFactoryTest {
+
+    @Mock
+    private Reflections reflections;
+
+    private PredicateInformationFactory predicateInformationFactory;
+
+    @Before
+    public void setUp() {
+        predicateInformationFactory = new PredicateInformationFactory(reflections);
+        Mockito.when(reflections.getMethodParamNames(Mockito.any(Method.class))).thenReturn(Arrays.asList("id", "date"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionOnMethodNotAnnotatedWithPredicate() throws Exception {
+        // given
+        // when
+        predicateInformationFactory.from(PredicateInformationFactoryTest.class.getMethod("testMethod", Long.class, LocalDateTime.class));
+        // then
+    }
+
+    @Test
+    public void shouldCreateSearchPredicateInformationFromMethod() throws Exception {
+        // given
+        final Method method = PredicateInformationFactoryTest.class.getMethod("testMethodWithAnnotation", Long.class, LocalDateTime.class);
+        // when
+        AbstractPredicate predicate = predicateInformationFactory.from(method);
+        // then
+        Assert.assertEquals(method.getName(), predicate.getName());
+        Assert.assertEquals(new ParameterInformation("id", Long.class), predicate.getParameters()[0]);
+        Assert.assertEquals(new ParameterInformation("date", LocalDateTime.class), predicate.getParameters()[1]);
+        Assert.assertEquals(SearchPredicate.class, predicate.getClass());
+        Assert.assertEquals(false, ((SearchPredicate) predicate).isDefaultedPageable());
+    }
+
+    @Test
+    public void shouldCreateFilterPredicateInformationFromMethod() throws Exception {
+        // given
+        final Method method = PredicateInformationFactoryTest.class.getMethod("testFilterPredicate", Long.class, LocalDateTime.class);
+        // when
+        AbstractPredicate predicate = predicateInformationFactory.from(method);
+        // then
+        Assert.assertEquals(method.getName(), predicate.getName());
+        Assert.assertEquals(new ParameterInformation("id", Long.class), predicate.getParameters()[0]);
+        Assert.assertEquals(new ParameterInformation("date", LocalDateTime.class), predicate.getParameters()[1]);
+        Assert.assertEquals(FilterPredicate.class, predicate.getClass());
+    }
+
+    @Test
+    public void shouldCreateSortPredicateInformationFromMethod() throws Exception {
+        // given
+        final Method method = PredicateInformationFactoryTest.class.getMethod("testSortPredicate", Long.class, LocalDateTime.class);
+        // when
+        AbstractPredicate predicate = predicateInformationFactory.from(method);
+        // then
+        Assert.assertEquals(method.getName(), predicate.getName());
+        Assert.assertEquals(new ParameterInformation("id", Long.class), predicate.getParameters()[0]);
+        Assert.assertEquals(new ParameterInformation("date", LocalDateTime.class), predicate.getParameters()[1]);
+        Assert.assertEquals(SortPredicate.class, predicate.getClass());
+    }
+
+    public void testMethod(Long id, LocalDateTime date) {
+
+    }
+
+    @Predicate(defaultedPageable = false, type = PredicateType.SEARCH)
+    public void testMethodWithAnnotation(Long id, LocalDateTime date) {
+
+    }
+
+    @Predicate(defaultedPageable = false, type = PredicateType.FILTER)
+    public void testFilterPredicate(Long id, LocalDateTime date) {
+
+    }
+
+    @Predicate(defaultedPageable = false, type = PredicateType.SORT)
+    public void testSortPredicate(Long id, LocalDateTime date) {
+
+    }
+
+    private static class Element {
+        private int a;
+        public Element next;
+
+        public Element(int a, Element next) {
+            this.a = a;
+            this.next = next;
+        }
+    }
+
+}
