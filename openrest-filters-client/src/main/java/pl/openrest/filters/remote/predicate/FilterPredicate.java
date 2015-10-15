143acd84915e01f1a579c3e2611f8e8fc4cf1fc5
konik32 <konickiszymon@gmail.com>
2015-10-15 14:30:29 +0200

    Remove toString Annotation from FilterPredicate class

--- openrest-filters-client/src/main/java/pl/openrest/filters/remote/predicate/FilterPredicate.java
+++ openrest-filters-client/src/main/java/pl/openrest/filters/remote/predicate/FilterPredicate.java
@@ -1,10 +1,8 @@
 package pl.openrest.filters.remote.predicate;
 
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @EqualsAndHashCode(callSuper = true)
-@ToString
 public class FilterPredicate extends AbstractPredicate {
 
     public FilterPredicate(String name, Object... parameters) {
