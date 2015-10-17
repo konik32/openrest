438d2c11318fd94acc658644bebed3e0df544c7c
konik32 <konickiszymon@gmail.com>
2015-10-17 16:59:34 +0200

    Add NonFinalOrStaticFieldFilter that excludes static or final fields

---
+++ openrest-rdto-generator/src/main/java/pl/openrest/rdto/generator/NonFinalOrStaticFieldFilter.java
@@ -0,0 +1,16 @@
+package pl.openrest.rdto.generator;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import org.springframework.util.ReflectionUtils.FieldFilter;
+
+public class NonFinalOrStaticFieldFilter implements FieldFilter {
+
+    @Override
+    public boolean matches(Field field) {
+        int modifiers = field.getModifiers();
+        return !(Modifier.isFinal(modifiers) || Modifier.isStatic(modifiers));
+    }
+
+}
