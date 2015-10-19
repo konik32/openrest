bc11847d20d58fb06ef8be195740c5c68d8483cb
konik32 <konickiszymon@gmail.com>
2015-10-19 13:21:00 +0200

    Remove unused classes (after updating to openrest-generator-commons) from openrest-rpr-generator

--- openrest-rpr-generator/src/main/java/pl/openrest/filters/generator/predicate/serializer/RemotePredicateRepositoryNamingStrategy.java
+++
@@ -1,7 +0,0 @@
-package pl.openrest.filters.generator.predicate.serializer;
-
-public interface RemotePredicateRepositoryNamingStrategy {
-
-    String getPackageName(String predicateRepositoryPackageName);
-    String getClassName(String predicateRepositoryClassName);
-}
