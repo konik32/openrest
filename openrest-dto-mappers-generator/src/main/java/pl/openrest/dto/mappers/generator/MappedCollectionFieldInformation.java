12bbb902d73d47cad188db447206e99f12ce3c49
konik32 <konickiszymon@gmail.com>
2015-11-13 14:35:12 +0100

    Add MappedFieldInformation and MappedCollectionFieldInformation that extracts information used to mapping from provided field

---
+++ openrest-dto-mappers-generator/src/main/java/pl/openrest/dto/mappers/generator/MappedCollectionFieldInformation.java
@@ -0,0 +1,36 @@
+package pl.openrest.dto.mappers.generator;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+
+import org.springframework.core.annotation.AnnotationUtils;
+
+import pl.openrest.dto.annotations.Dto;
+
+public class MappedCollectionFieldInformation extends MappedFieldInformation {
+
+    public MappedCollectionFieldInformation(Field field) {
+        super(field);
+        if (!Collection.class.isAssignableFrom(field.getType()))
+            throw new IllegalArgumentException(String.format("%s is not a Collection", toString()));
+    }
+
+    public Class<?> getRawType() {
+        return (Class<?>) ((ParameterizedType) (Type) getType()).getActualTypeArguments()[0];
+    }
+
+    @Override
+    public boolean isDto() {
+        return AnnotationUtils.getAnnotation(getRawType(), Dto.class) != null;
+    }
+
+    @Override
+    public Class<?> getEntityType() {
+        if (!isDto())
+            throw new IllegalStateException(String.format("%s field raw type is not dto", toString()));
+        return super.getEntityType();
+    }
+
+}
---
+++ openrest-dto-mappers-generator/src/main/java/pl/openrest/dto/mappers/generator/MappedFieldInformation.java
@@ -0,0 +1,76 @@
+package pl.openrest.dto.mappers.generator;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+
+import org.springframework.util.ReflectionUtils;
+
+import pl.openrest.dto.annotations.Dto;
+import pl.openrest.dto.annotations.Nullable;
+
+public class MappedFieldInformation {
+
+    protected final PropertyDescriptor propertyDescriptor;
+    protected final Field field;
+
+    public MappedFieldInformation(Field field) {
+        try {
+            this.field = field;
+            this.propertyDescriptor = new PropertyDescriptor(field.getName(), getDeclaringClass());
+        } catch (IntrospectionException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public String getGetterName() {
+        return propertyDescriptor.getReadMethod().getName();
+    }
+
+    public String getSetterName() {
+        return propertyDescriptor.getReadMethod().getName();
+    }
+
+    public boolean isNullable() {
+        return field.getAnnotation(Nullable.class) != null;
+    }
+
+    public Class<?> getType() {
+        return field.getType();
+    }
+
+    public Class<?> getDeclaringClass() {
+        return field.getDeclaringClass();
+    }
+
+    public boolean isDto() {
+        return field.getAnnotation(Dto.class) != null;
+    }
+
+    public Class<?> getEntityType() {
+        if (!isDto())
+            throw new IllegalStateException(String.format("%s field type is not dto", toString()));
+        return field.getAnnotation(Dto.class).entityType();
+    }
+
+    public String getNullableGetterName() {
+        if (!isNullable())
+            throw new IllegalStateException("Field is not nullable");
+        Nullable nullable = field.getAnnotation(Nullable.class);
+        Field nullableField = ReflectionUtils.findField(getDeclaringClass(), nullable.value());
+        if (nullableField == null)
+            throw new IllegalStateException(String.format("%s doesn't have %s property", getDeclaringClass(), nullableField));
+        try {
+            PropertyDescriptor nullablePropertyDescriptor = new PropertyDescriptor(nullable.value(), getDeclaringClass());
+            return nullablePropertyDescriptor.getReadMethod().getName();
+        } catch (IntrospectionException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s.%s", getDeclaringClass(), getType());
+    }
+
+}
